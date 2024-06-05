5bce83657b50edd01cee86b3ae824dc36d54d04e
jisu <6039js@naver.com>
2024-06-05 17:47:48 +0900

    사용자 프로필 수정

--- src/main/java/CodeMaker/togetherLion/domain/review/repository/ReviewRepository.java
+++ src/main/java/CodeMaker/togetherLion/domain/review/repository/ReviewRepository.java
@@ -14,7 +14,7 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
     List<Review> findByPost_PostIdIn(List<Integer> postIds);
 
-    @Query("SELECT ROUND(AVG(r.starScore), 1) FROM Review r " +
+    @Query("SELECT COALESCE(ROUND(AVG(r.starScore), 1), 0) FROM Review r " +
             "INNER JOIN Post p ON p.postId = r.post.postId " +
             "WHERE p.user.userId = :userId")
     double getAvgStarScore(@Param("userId") int userId);
