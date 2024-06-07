d83d1b3e62a16b60fe769df159ad27c76fa2dd5d
parkha <ugauga19892556@gmail.com>
2024-06-07 09:34:52 +0900

    수정

--- src/main/java/CodeMaker/togetherLion/domain/chat/dto/ChatRes.java
+++ src/main/java/CodeMaker/togetherLion/domain/chat/dto/ChatRes.java
@@ -1,6 +1,7 @@
 package CodeMaker.togetherLion.domain.chat.dto;
 
 import CodeMaker.togetherLion.domain.chat.entity.Chat;
+import CodeMaker.togetherLion.domain.post.dto.PostRes;
 import CodeMaker.togetherLion.domain.waitingdeal.entity.WaitingDeal;
 import CodeMaker.togetherLion.domain.waitingdeal.model.WaitingState;
 
@@ -10,11 +11,13 @@ public record ChatRes (
         int ChatRoomId,
         String roomId,
         LocalDateTime createChatRoom,
-        String chatroomName
+        String chatroomName,
+        int userId
+        //PostRes postRes // 추가된 부분
 
 ){
 public static ChatRes from(Chat chat) {
-    return new ChatRes(chat.getChatRoomId(), chat.getRoomId(), chat.getCreateChatRoom(), chat.getChatroomName());
+    return new ChatRes(chat.getChatRoomId(), chat.getRoomId(), chat.getCreateChatRoom(), chat.getChatroomName(), chat.getPost().getUser().getUserId());
 }
 }
 
